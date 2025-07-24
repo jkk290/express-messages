c16afdc3c28ef48eef664f4e9bc555743ffeee41
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-07-23 16:49:40 -1000

    Add function to view individual message details

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true}));
 
 app.use('/', indexRouter);
-app.use('/new', indexRouter);
 
 app.listen(PORT, () => {
     console.log('Message app listening on port ', PORT);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,16 +3,22 @@ const { Router } = require('express');
 const indexRouter = Router();
 const messages = [
     {
+        id: 1,
         text: 'Hi There!',
         user: 'Amanda',
         added: new Date()
     },
     {
+        id: 2,
         text: 'Hello world!',
         user: 'Charles',
         added: new Date()
     }
 ];
+let count = 3;
+const getIndex = (id) => {
+    return messages.findIndex((message) => message.id === id );
+};
 
 indexRouter.get('/', (req, res) => {
     res.render('index', { title: 'Mini Messageboard', messages: messages });
@@ -20,8 +26,14 @@ indexRouter.get('/', (req, res) => {
 indexRouter.get('/new', (req, res) => {
     res.render('newMessage');
 });
+indexRouter.get('/:messageId', (req, res) => {
+    const id = parseInt(req.params.messageId);
+    const index = getIndex(id);
+    res.render('message.ejs', { message: messages[index] });
+});
 indexRouter.post('/new', (req, res) => {
-    messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+    messages.push({ id: count, text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+    count += 1;
     res.redirect('/');
 });
 
