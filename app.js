b6614ee35ce6be42d69226532f06dd8a5e213d3e
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-07-23 21:28:46 -1000

    Add styling to pages

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const indexRouter = require('./routes/indexRouter.js');
 
 const app = express();
 const PORT = 3000;
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
