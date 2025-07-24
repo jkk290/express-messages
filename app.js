2b632e080e5f95253df6abbbe6532c6837656f5e
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-07-23 15:41:43 -1000

    Implement adding new messages

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const PORT = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true}));
 
 app.use('/', indexRouter);
+app.use('/new', indexRouter);
 
 app.listen(PORT, () => {
     console.log('Message app listening on port ', PORT);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -17,5 +17,12 @@ const messages = [
 indexRouter.get('/', (req, res) => {
     res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+indexRouter.get('/new', (req, res) => {
+    res.render('newMessage');
+});
+indexRouter.post('/new', (req, res) => {
+    messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+    res.redirect('/');
+});
 
 module.exports = indexRouter;
